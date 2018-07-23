59d431befa8befd9054c0bc1c063542d90ac02cc
dwdy250 <timdowd1984@gmail.com>
2018-07-23 21:51:44 +0100

    stock picker finished

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,25 +1,27 @@
 #Implement a method #stock_picker that takes in an array of stock prices, one for each hypothetical day. It should return a pair of days representing the best day to buy and the best day to sell. Days start at 0.
 #You need to buy before you can sell
 #Pay attention to edge cases like when the lowest day is the last day or the highest day is the first day.
-#price_difference = []
+
 
 def stock_picker(prices)
   price_diff = 0
-  # go through prices working out difference between others
+  count = 0
+  best_days_array = []
+
   prices.each do |price1|
-
+    count += 1
     prices.each do |price2|
-      (price2 - price1) > price_diff ? price_diff = (price2 - price1) : price_diff
-      puts price_diff
-
+      if count > prices.index(price2)
+        next
+      else
+        if (price2 - price1) > price_diff
+          price_diff = (price2 - price1)
+          best_days_array = [prices.index(price1), prices.index(price2)]
+        end
+      end
     end
-    puts "-"
   end
-
-
-
-  puts price_diff
-
+  puts best_days_array
 end
 
-stock_picker([10,5,15,2,7])
+stock_picker([5,15,4,16,1,1,1,22])
